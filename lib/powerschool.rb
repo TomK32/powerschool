9be39339067d18cc9afe393232f4726f4e64eb0d
Thomas R. Koll <tomk32@tomk32.de>
2015-04-15 15:11:26 +0200

    Add a few more routes

--- lib/powerschool.rb
+++ lib/powerschool.rb
@@ -23,9 +23,15 @@ class Powerschool
     end
   end
 
-  get :districts, '/district'
-  get :schools, '/school'
+  # client is set up per district so it returns only one district
+  # for urls with parameters
+  get :district, '/district'
+  get :schools, '/district/school'
   get :teachers, '/staff'
-  get :students, '/students'
+  get :students, '/student'
   get :sections, '/section'
+  get :school_sections, '/school/:school_id/section'
+  get :school_students, '/school/:school_id/student'
+
+  get :metadata, '/metadata'
 end
